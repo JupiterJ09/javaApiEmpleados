8f92ab8c121874ca346f4cfe8ad902f7004d6880
Anibal Cabrera <jose.cabrera78@unach.mx>
2025-09-25 12:07:27 -0600

    primer commit

---
+++ src/main/java/com/schoolemployeeapi/schoolemployeeapi/SchoolemployeeapiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolemployeeapi.schoolemployeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolemployeeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolemployeeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolemployeeapi/schoolemployeeapi/SchoolemployeeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolemployeeapi.schoolemployeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolemployeeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
